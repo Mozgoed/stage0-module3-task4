69315b4850fd4519e1cfdf5008a5dc8f45e00ae9
Кононов Алексей <aleksey.n.kononov@gazprombank.ru>
2023-07-07 14:37:52 +0300

    done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        int charC = c;
+        int charH = h;
+        int charA = a;
+        int charR = r;
+
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
